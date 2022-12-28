548a3de698daea4e30e7a1d6c34a400d0684fdcc
Abhishek Dubey <7abhishek1410@gmail.com>
2022-12-28 19:51:12 +0530

    updates

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,9 +5,13 @@ import Navbar from './Navbar'
 const Home = () => {
   return (
     <>
-    
+    <div className='h-[100vh]' style={{
+        backgroundImage: "linear-gradient(to bottom left, #0F172A 0%, #162F49 20%, #0F172A 100%)",
+      }}>
+      
       <Navbar/>
       <Outlet/>
+    </div>
     
     </>
   )
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,11 +1,33 @@
 import React from "react";
-import "bootstrap/dist/css/bootstrap.css";
+import { FaConnectdevelop } from "react-icons/fa";
 import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <>
-    
+        <div className="flex flex-row px-10 py-4 justify-between items-center text-white" >
+
+          {/* logo */}
+
+          <div >
+           <FaConnectdevelop className="text-5xl  font-extrabold"/>
+          </div>
+
+        {/* list */}
+
+          <div>
+            <ul className="flex gap-8 text-xl font-bold list-none">
+              <li>
+                <NavLink to="/about"  >About</NavLink>
+              </li>
+              <li>
+                <NavLink to="/contact">Contact</NavLink>
+              </li>
+           
+            </ul>
+          </div>
+
+        </div>
     </>
   );
 };
