71ac8f94d240afa5a77ae6753d7c0ee6262c85c8
Abhishek Dubey <7abhishek1410@gmail.com>
2022-12-30 19:05:43 +0530

    integration-updates

--- src/Components/LoginModal.js
+++ src/Components/LoginModal.js
@@ -1,9 +1,32 @@
 import React from 'react'
 import { ImCancelCircle } from "react-icons/im";
+import { useState } from 'react';
 
 const LoginModal = ({closeModal}) => {
+
+  const [inputs, setInputs] = useState({
+     email: "", password:""
+  })
+
+  let name, value
+
+
+  const handleInputs = (e) => {
+    name = e.target.name
+    value = e.target.value
+    
+    setInputs({...inputs, [name]:value})
+  }
+
+
     const handleOnClick  = ()=>{
         closeModal(false)
+    }
+
+    const handleSubmit = (e) =>{
+      e.preventdefault()
+      
+      
     }
   return (
     <>
@@ -15,17 +38,25 @@ const LoginModal = ({closeModal}) => {
                         <ImCancelCircle className='text-white text-xl hover:text-red-500' />
                         </button>
                 {/* Modal div */}
-                    <form action="/" className='w-[30vw] h-[50vh] border rounded-xl flex flex-col gap-10 items-center justify-center px-8 bg-gray-400  backdrop-filter backdrop-blur-sm bg-opacity-10 -z-10 text-white'>
+                    <form action="/" 
+                    onSubmit={handleSubmit}
+                    className='w-[30vw] h-[50vh] border rounded-xl flex flex-col gap-10 items-center justify-center px-8 bg-gray-400  backdrop-filter backdrop-blur-sm bg-opacity-10 -z-10 text-white'>
 
 
                         <p className='text-2xl text-center text-white'>Sign in to continue...</p>
 
-                        <input type="email"  placeholder="Your Name" className='py-3 px-8 outline-none  min-w-full rounded-xl bg-gray-400 placeholder-white caret-white' />
+                        <input type="email"  placeholder="Your Email" className='py-3 px-8 outline-none  min-w-full rounded-xl bg-gray-400 placeholder-white caret-white'
+                        name='email'
+                        value={inputs.email}
+                        onChange={handleInputs} />
                         
-                        <input type="email"  placeholder="Password" className='py-3 px-8 outline-none bg-gray-400 placeholder-white min-w-full rounded-xl caret-white' />
+                        <input type="password"  placeholder="Password" className='py-3 px-8 outline-none bg-gray-400 placeholder-white min-w-full rounded-xl caret-white'
+                         name='password'
+                         value={inputs.password}
+                         onChange={handleInputs} />
 
                     
-              <button className='rounded-xl bg-gray-400 min-w-[12vw]  hover:bg-[#0F172A]'>
+              <button type='submit' className='rounded-xl bg-gray-400 min-w-[12vw]  hover:bg-[#0F172A]'>
                 <p className='text-white  p-2 tracking-wide font-bold '>Login</p>
               </button>
 
--- src/Components/SignupModal.js
+++ src/Components/SignupModal.js
@@ -1,12 +1,80 @@
 import React from "react";
 import { ImCancelCircle } from "react-icons/im";
+import { useState } from "react";
+import {useNavigate} from "react-router-dom"
 
-const SignupModal = ({closeModal}) => {
+const SignupModal = ({ closeModal }) => {
 
-    const handleOnClick = () => {
-        closeModal(false)
+  const navigate = useNavigate();
+
+  const [inputs, setInputs] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    passion: "",
+    password: "",
+    cnfPassword: "",
+  });
+  let name, value;
+  const handleInputs = (e) => {
+    name = e.target.name;
+    value = e.target.value;
+    setInputs({ ...inputs, [name]: value });
+  };
+
+  const handleSubmit = async (e) => {
+    // window.alert("jkhgfdsgghjkl")
+    e.preventdefault();
+    // console.log("hi")
+
+    const { name, email, phone, passion, password, cnfPassowrd } = inputs;
+
+
+    window.alert(name)
+      
+    const res = fetch("/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name,
+        email,
+        phone,
+        passion,
+        password,
+        cnfPassowrd,
+      }),
+    });
+
+    console.log("hiiouyghgf")
+
+
+    const data = await res.json();
+
+    console.log("hi no")
+
+
+    if (data.status === 422 || !data) {
+      window.alert("Invalid Registration");
+      console.log("Invalid Registration");
+    console.log("hvhvhtvi")
+
+    } else {
+      window.alert(" Registration Successful");
+      console.log("Registration Successful");
+    console.log("hi from kjhg")
+      
+      navigate("/")
     }
 
+    console.log("hi09876")
+
+  };
+
+  const handleOnClick = () => {
+    closeModal(false);
+  };
 
   return (
     <>
@@ -21,8 +89,9 @@ const SignupModal = ({closeModal}) => {
         </button>
         {/* Modal div */}
         <form
-          action="/"
-          className="w-[30vw]   border rounded-xl flex flex-col gap-8 items-center justify-center px-8 py-8 bg-gray-400  backdrop-filter backdrop-blur-sm bg-opacity-10 -z-10 text-white"
+          
+     
+          className="w-[30vw]  border rounded-xl flex flex-col gap-8 items-center justify-center px-8 py-8 bg-gray-400  backdrop-filter backdrop-blur-sm bg-opacity-10 -z-10 text-white"
         >
           <p className="text-2xl text-center text-white">
             Sign up to connect...
@@ -31,38 +100,58 @@ const SignupModal = ({closeModal}) => {
           <input
             type="text"
             placeholder="Your Name"
-            className="py-3 px-8 outline-none  min-w-full rounded-xl bg-gray-400 placeholder-white  caret-white   "
-       />
+            className="py-3 px-8 outline-none  min-w-full rounded-xl bg-gray-400 placeholder-white  caret-white"
+            name="name"
+            value={inputs.name}
+            onChange={handleInputs}
+          />
 
           <input
             type="email"
             placeholder="Your Email"
             className="py-3 px-8 outline-none bg-gray-400 placeholder-white min-w-full rounded-xl caret-white"
+            name="email"
+            value={inputs.email}
+            onChange={handleInputs}
           />
           <input
             type="tel"
             placeholder="Mobile Number"
             className="py-3 px-8 outline-none bg-gray-400 placeholder-white min-w-full rounded-xl caret-white"
+            name="phone"
+            value={inputs.phone}
+            onChange={handleInputs}
           />
           <input
             type="text"
-            placeholder="Profession"
+            placeholder="Passion"
             className="py-3 px-8 outline-none bg-gray-400 placeholder-white min-w-full rounded-xl caret-white"
+            name="passion"
+            value={inputs.passion}
+            onChange={handleInputs}
           />
           <input
             type="password"
             placeholder="Password"
             className="py-3 px-8 outline-none bg-gray-400 placeholder-white min-w-full rounded-xl caret-white"
+            name="password"
+            value={inputs.password}
+            onChange={handleInputs}
           />
           <input
             type="password"
             placeholder="Confirm Password"
             className="py-3 px-8 outline-none bg-gray-400 placeholder-white min-w-full rounded-xl caret-white"
+            name="cnfPassword"
+            value={inputs.cnfPassword}
+            onChange={handleInputs}
           />
 
-          
-
-          <button className="rounded-xl bg-gray-400 min-w-[12vw]  hover:bg-[#0F172A]">
+          <button
+            type="submit"
+            className="rounded-xl bg-gray-400 min-w-[12vw]  hover:bg-[#0F172A]"
+            onClick={()=> handleSubmit()}
+          >
             <p className=" text-white  p-2 tracking-wide font-bold ">
               Register
             </p>
