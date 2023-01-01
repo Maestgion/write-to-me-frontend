b6f4e195bba4bc0f4f82ef0942a6f43e88b2082f
Abhishek Dubey <7abhishek1410@gmail.com>
2023-01-01 15:51:13 +0530

    contact-form-updates

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -10,7 +10,9 @@ const Contact = () => {
 
   const [userData, setUserData] = useState({});
 
-  
+  const [contactData, setContactData] = useState({
+    name:"", email:"", phone: "", message:""
+  })
 
   const callContactPage= async ()=> {
 
@@ -44,6 +46,48 @@ const Contact = () => {
         callContactPage();
   }, [])
   
+  
+  let name, value;
+  const handleChange =  (e) =>{
+  name = e.target.name;
+  value = e.target.value;
+
+  setContactData({...contactData, [name]:value});
+
+  } 
+
+
+  const handleSubmit = async (e) =>{
+    e.preventDefault()
+
+    const {name, email, phone, message} = contactData;
+
+    const res = await fetch("/contact", {
+      method: "POST",
+      headers:{
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        name,
+        email,
+        phone,
+        message
+      })
+    });
+
+
+    const data = await res.json()
+    console.log(data)
+
+    if(!data===201)
+    {
+      console.log("Invalid data")
+    }
+    else{
+      window.alert("message sent successfully");
+    }
+
+  }
 
   return (
     <>  
@@ -54,7 +98,7 @@ const Contact = () => {
       <Navbar/>
 
        {/* container */}
-       <div className='px-10 flex flex-col items-center gap-16'>
+       <div className='px-10 flex flex-col items-center gap-12'>
            
 
            {/* basic cards */}
@@ -67,22 +111,32 @@ const Contact = () => {
    
            {/* contact form */}
    
-          <div className='w-[70vw] p-10 flex flex-col justify-start bg-gray-400 backdrop-filter backdrop-blur-sm bg-opacity-10 rounded-xl border border-gray-100 gap-6'>
+          <div className='w-[70vw] px-10 py-6 flex flex-col justify-start bg-gray-400 backdrop-filter backdrop-blur-sm bg-opacity-10 rounded-xl border border-gray-100 gap-6'>
    
              <div>
                <p className='text-2xl font-semibold text-white'>Get in Touch</p>
              </div>
    
-          <form action='/' className='flex flex-col gap-10 justify-center  '>
+          <form method='POST' onSubmit={handleSubmit} className='flex flex-col gap-10 justify-center  '>
    
            <div className='w-full caret-white flex gap-10'>
-             <input type="text" value={userData.name} className='bg-gray-400 backdrop-filter backdrop-blur-sm bg-opacity-10 rounded-xl outline-none px-8 py-4 placeholder-white w-[20vw] text-white' placeholder='Your Name'  />
-             <input type="email" value={userData.email} className='bg-gray-400 backdrop-filter backdrop-blur-sm bg-opacity-10 rounded-xl outline-none px-8 py-4 placeholder-white w-[20vw] text-white' placeholder='Your Email'  />
-             <input type="tel" value={userData.phone} className='bg-gray-400 backdrop-filter backdrop-blur-sm bg-opacity-10 rounded-xl outline-none px-8 py-4 placeholder-white w-[20vw] text-white' placeholder='Your Number'  />
+             <input type="text" name="name" value={userData.name}
+             onChange={handleChange} className='bg-gray-400 backdrop-filter backdrop-blur-sm bg-opacity-10 rounded-xl outline-none px-8 py-4 placeholder-white w-[20vw] text-white' placeholder='Your Name'  />
+             <input type="email" name="email" value={userData.email} onChange={handleChange} className='bg-gray-400 backdrop-filter backdrop-blur-sm bg-opacity-10 rounded-xl outline-none px-8 py-4 placeholder-white w-[20vw] text-white' placeholder='Your Email'  />
+             <input type="tel" name="phone" value={userData.phone} onChange={handleChange} className='bg-gray-400 backdrop-filter backdrop-blur-sm bg-opacity-10 rounded-xl outline-none px-8 py-4 placeholder-white w-[20vw] text-white' placeholder='Your Number'  />
            </div>
    
-           <textarea name="message" id="msg" cols="30" rows="8" className='bg-gray-400 backdrop-filter backdrop-blur-sm bg-opacity-10 rounded-xl outline-none placeholder-white caret-white text-white px-8 py-4'></textarea>
-   
+           <textarea name="message" id="msg" cols="30" rows="6" value={contactData.message} onChange={handleChange} className='bg-gray-400 backdrop-filter backdrop-blur-sm bg-opacity-10 rounded-xl outline-none placeholder-white caret-white text-white px-8 py-4'></textarea>
+
+           <button
+            type="submit"
+            className="rounded-xl bg-gray-400 w-[12vw]  hover:bg-[#0F172A]"
+           
+          >
+            <p className=" text-white  p-2 tracking-wide font-bold ">
+              Send
+            </p>
+          </button>
          </form>
           </div>
            
